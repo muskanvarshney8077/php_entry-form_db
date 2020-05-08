3fa4da0a04330b40b33b0405f831a2b3831121e8
muskanvarshney8077 <64691822+muskanvarshney8077@users.noreply.github.com>
2020-05-08 18:13:16 +0530

    Update task1_entry.php

--- task1_entry.php
+++ task1_entry.php
@@ -18,39 +18,7 @@ if(!$db_server)die("unable to connect mysql server".mysql_error());
 mysql_select_db($db_database)
 or
 die("unable to select db".mysql_error());
-echo <<<_end
 
-<form action="task1_entry.php" method="post">
-<div class="form">
-  <div class="form-group">
-    <label for="name">Name</label>
-    <input type="text" class="form-control" id="exampleInputName" aria-describedby="nameHelp" placeholder="Enter name" name="name">
-    <small id="nameHelp" class="form-text text-muted">We will never share your details with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="phone_number">Phone number</label>
-    <input type="number" class="form-control" id="exampleInputPhone_number" placeholder="Enter Phone number" name="phone_number">
-  </div>
-  <div class="form-group">
-    <label for="city">City</label>
-    <input type="text" class="form-control" id="exampleInputCity" placeholder="Enter city" name="city">
-  </div>
-  <div class="form-group">
-    <label for="country">Country</label>
-    <input type="text" class="form-control" id="exampleInputCountry" placeholder="Enter country" name="country">
-  </div>
-  <div class="form-group">
-    <label for="email">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail" placeholder="Enter email" name="email">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
-_end;
 if(isset($_POST['name'])&&isset($_POST['phone_number'])&&isset($_POST['city'])&&isset($_POST['country'])&&isset($_POST['email']))
 {
 	$name=($_POST['name']);
